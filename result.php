77574fd0a10a5d2a10ac07db238a0770e75987af
Nestor_Syvuliak <nestor.syvulyak@gmail.com>
2020-12-03 18:54:25 +0200

    add files

---
+++ result.php
@@ -0,0 +1,26 @@
+<?php 
+
+if(isset($_POST['name'])){
+
+$dir    = $_POST['name'];
+if($dir == ""){echo "Error #1: Enter any folder path"; exit;}
+$files1 = scandir($dir);
+$count1 = count(scandir($dir)); // number of files in directory
+
+$loc = "Folder path: \"".$dir."\"\r\n----------------";
+
+$fp = fopen("file11.txt", "w");
+fwrite($fp, $loc."\r\n");
+	for($i = 0; $i < $count1; $i++){
+		$test321 = $files1[$i];
+		fwrite($fp, $i.". ".$test321."\r\n");
+	}
+fclose($fp);
+
+}
+
+echo "File with name \"file11.txt\" created in directory with projects files. ";
+
+
+
+?>
---
+++ task.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>MyTask</title>
+ </head>
+ <body>
+  	<form action="result.php" method="POST">
+  	<h2>Enter Folder path:</h2>
+	<input type="text" name="name" placeholder="Enter folder path"><br><br>
+	<input type="submit" name="submit">
+	</form>
+ </body>
+</html>
