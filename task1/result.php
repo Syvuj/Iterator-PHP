33a38a7a3674133cd0a739579185266b52c392de
Nestor_Syvuliak <nestor.syvulyak@gmail.com>
2020-12-04 16:09:10 +0200

    recursive iterator

--- task1/result.php
+++ task1/result.php
@@ -2,25 +2,23 @@
 
 if(isset($_POST['name'])){
 
-$dir    = $_POST['name'];
-if($dir == ""){echo "Error #1: Enter any folder path"; exit;}
-$files1 = scandir($dir);
-$count1 = count(scandir($dir)); // number of files in directory
-
-$loc = "Folder path: \"".$dir."\"\r\n----------------";
+$dir = $_POST['name'];
+$items = new RecursiveDirectoryIterator($dir,RecursiveDirectoryIterator::SKIP_DOTS);
+$iter = new RecursiveIteratorIterator($items,RecursiveIteratorIterator::SELF_FIRST,RecursiveIteratorIterator::CATCH_GET_CHILD);
 
 $fp = fopen("file11.txt", "w");
+$loc = "Folder path: \"".$dir."\"\r\n----------------";
 fwrite($fp, $loc."\r\n");
-	for($i = 0; $i < $count1; $i++){
-		$test321 = $files1[$i];
-		fwrite($fp, $i.". ".$test321."\r\n");
+	foreach ($iter as $file) {
+		$sd = $file->getPathname();
+		fwrite($fp, $sd."\r\n");
 	}
 fclose($fp);
-
+echo "File with name \"file11.txt\" created in directory with project files. ";
 }
 
-echo "File with name \"file11.txt\" created in directory with project files. ";
 
 
-
+// $files1 = scandir($dir);
+// $count1 = count(scandir($dir)); // number of files in directory
 ?>
